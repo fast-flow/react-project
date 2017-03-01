167bf0a2831127ca15fc22abafeb60fa693ea0f2
Nimo <nimo.jser@gmail.com>
2017-03-01 11:25:22 +0800

    php render

--- template/mock/example.js
+++ template/mock/example.js
@@ -1,6 +1,15 @@
 module.exports = function (app) {
     app.view({
         url: '/example',
-        template: '/example/index.html'
+        template: '/example/index.html',
+        data: {
+            name: 'nimo',
+            data: {
+                arr: [1,2,3],
+                obj: {name: 'some'},
+                number:1,
+                string: "abc"
+            }
+        }
     })
 }
