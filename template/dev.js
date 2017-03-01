4816111f6aaf3866080bd53acee745f94e00eda3
Nimo <nimo.jser@gmail.com>
2017-03-01 16:03:00 +0800

    dev.js

--- template/dev.js
+++ template/dev.js
@@ -1,5 +1,35 @@
+class Entry {
+    '/example' () { require(['./view/example/app'], renderComponent()) }
+}
+require('./view/common/entry.js')
+
+// =============================
 import { Component } from "react"
 import { render } from "react-dom"
 import Example from "./view/example/app"
+import extend from "extend"
 
-render(<Example />, document.getElementById('app'))
+if (typeof Entry.prototype[location.pathname] === 'function') {
+    Entry.prototype[location.pathname]()
+}
+else {
+    console.log('/dev.js not match ' + location.pathname)
+}
+
+function renderComponent (settings) {
+    var defaultSettins = {
+        renderNode: document.getElementById('app'),
+        props: {}
+    }
+    settings = extend(true, defaultSettins, settings)
+    return function (Component) {
+       let App = Component
+       if (typeof Component.default !== 'undefined') {
+           App = Component.default
+       }
+       if (typeof settings.renderNode === 'undefined') {
+           throw new Error('/dev.js not find renderNode')
+       }
+       render(<App {...settings.props} />, settings.renderNode)
+    }
+}
