f9a290f4ea684f2057e767c8b5980d25a6cb6d8f
Nimo <nimo.jser@gmail.com>
2017-03-02 17:38:29 +0800

    If you change begin with webpack properties

--- template/local-config.js
+++ template/local-config.js
@@ -1,5 +1,11 @@
-// Don't git commit this file
+/**
+ * Don't git commit this file
+ */
 module.exports ={
+    // If you change begin with webpack properties
+    // Run the command again `npm run js` or `npm run js:debug`
+    // 如果你修改了以 webpackNoCompile 开头的文件
+    // 你需要重新运行 `npm run js` 或 `npm run js:debug`
     webpackNoCompile: [
         // './view/example/**'
         // './m/**'
