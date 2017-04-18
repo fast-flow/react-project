115e3bd555e687f4979be3a018e92dd6814e7880
Nimo <nimo.jser@gmail.com>
2017-04-18 10:16:48 +0800

    online:debug

---
+++ template/config/webpack.online.debug.js
@@ -0,0 +1,1 @@
+module.exports = require('./webpack.online').getConfig({debug: true})
--- template/config/webpack.online.js
+++ template/config/webpack.online.js
@@ -8,32 +8,43 @@ onlineEntry.forEach(function (path) {
     onlineEntryMap[path.replace(/\.js$/,'')] = './' + path
 })
 
-var webpackConfig = require('./webpack.base')({
-    externals: userConfig.webpackExternals,
-    entry: onlineEntryMap,
-    lastPlugins: [
-       new webpack.DefinePlugin({
-           'process.env.NODE_ENV': JSON.stringify('production')
-       }),
-       new webpack.optimize.UglifyJsPlugin({
-           fromString: true,
-           compress: {
-               warnings: false,
-               screw_ie8: false
-           },
-           mangle: {
-               screw_ie8: false
-           },
-           output: {
-               screw_ie8: false
-           }
-       })
-    ],
-    output: {
-        path: path.join(__dirname, '../output'),
-        filename: '[name].js',
-        publicPath: userConfig.domain,
-        chunkFilename: '__chunk/[name]-[id]-[hash]-chunk.js'
+var getConfig  = function (settings) {
+    settings = settings || {}
+    var uglifyPlugin = []
+    if (!settings.debug) {
+        uglifyPlugin.push(
+            new webpack.optimize.UglifyJsPlugin({
+                fromString: true,
+                compress: {
+                    warnings: false,
+                    screw_ie8: false
+                },
+                mangle: {
+                    screw_ie8: false
+                },
+                output: {
+                    screw_ie8: false
+                }
+            })
+        )
     }
-})
-module.exports = webpackConfig
+    return require('./webpack.base')({
+        externals: userConfig.webpackExternals,
+        entry: onlineEntryMap,
+        devtool: 'source-map',
+        lastPlugins: [
+           new webpack.DefinePlugin({
+               'process.env.NODE_ENV': JSON.stringify('production')
+           })
+        ].concat(uglifyPlugin)
+        ,
+        output: {
+            path: path.join(__dirname, '../output'),
+            filename: '[name].js',
+            publicPath: userConfig.domain,
+            chunkFilename: '__chunk/[name]-[id]-[hash]-chunk.js'
+        }
+    })
+}
+module.exports = getConfig()
+module.exports.getConfig = getConfig
