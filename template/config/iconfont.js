351f3ea217d770af565a1b2cbcd64789172f18a9
Grifree <1149290648@qq.com>
2017-07-06 16:42:56 +0800

    config/iconfont 文件路径

--- template/config/iconfont.js
+++ template/config/iconfont.js
@@ -1,6 +1,6 @@
 var fs = require('fs')
 var path = require('path')
-var filepath = path.join(__dirname, '../m/icons/index.less')
+var filepath = path.join(__dirname, '../m/icons/iconfont.css')
 
 var css = fs.readFileSync(filepath, 'utf-8').toString()
 var lessSource = css.replace(/\.icon-(.*):before\s*{\s*content:\s*"([^"]+)";\s*}/g, [
@@ -15,6 +15,6 @@ var lessSource = css.replace(/\.icon-(.*):before\s*{\s*content:\s*"([^"]+)";\s*}
 console.log('------ less ---------')
 console.log(lessSource)
 console.log('------ less ---------')
-var dest = path.join(filepath)
+var dest = path.join(filepath, '../index.less')
 fs.writeFileSync(dest, lessSource, 'utf-8')
 console.log(dest + ' writeFile done!')
