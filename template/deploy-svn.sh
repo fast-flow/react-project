88a5c9c338fbde30943aff437217ebd754ab568b
Nimo <nimo.jser@gmail.com>
2017-07-10 23:28:43 +0800

    Update deploy-svn.sh

--- template/deploy-svn.sh
+++ template/deploy-svn.sh
@@ -3,6 +3,8 @@ svn co https://svn.duapp.com/appidda1vdfz59m --username process.env.SVN_USERNAME
 cd ./appidda1vdfz59m
 svn update
 cd ../
+## `npm install -g cpy-cli`
+## or `yarn global add cpy-cli`
 cpy 'view/**/*.html' '../appidda1vdfz59m'  --cwd=output --parents
 cd ./appidda1vdfz59m
 svn add * --force
