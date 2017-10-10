e4d0fb2e83398448a4bdbe846f92afda833e7b57
Grifree <1149290648@qq.com>
2017-10-10 15:06:53 +0800

    use switch

--- template/dev.js
+++ template/dev.js
@@ -1,31 +1,28 @@
-require('./view/common/entry.js')
-class Entry {
-     '/example' () {
-        require(['./view/example/app'], renderComponent())
-     }
-     '/view/example/index.html' () {
-        require(['./view/example/app'], renderComponent())
-     }
-     '/view/example-router/index.html' () {
-        require(['./view/example-router/app'], renderComponent())
-     }
-    '/m/example/README.html' () {
-        require(['./m/example/index.demo.js'], renderComponent())
-     }
-     '/view/redux/index.html' () {
-         require(['./view/redux/app'], renderComponent())
-     }
-}
-
-// =============================
 import { Component } from "react"
 import { render } from "react-dom"
 import extend from "extend"
 
-if (typeof Entry.prototype[location.pathname] === 'function') {
-    Entry.prototype[location.pathname]()
-}
-else {
+// common script
+require('./view/common/entry.js')
+
+// entry
+switch(location.pathname) {
+    case '/example':
+        require(['./view/example/app'], renderComponent())
+    break
+    case '/view/example/index.html':
+        require(['./view/example/app'], renderComponent())
+    break
+    case '/view/example-router/index.html':
+        require(['./view/example-router/app'], renderComponent())
+    break
+    case '/m/example/README.html':
+        require(['./m/example/index.demo.js'], renderComponent())
+    break
+    case '/view/redux/index.html':
+        require(['./view/redux/app'], renderComponent())
+    break
+    default:
     console.log('/dev.js not match ' + location.pathname)
 }
 
@@ -39,13 +36,8 @@ function renderComponent (settings) {
         settings.renderNode = document.getElementById(settings.renderNode)
     }
     return function (Component) {
-       let App = Component
-       if (typeof Component.default !== 'undefined') {
-           App = Component.default
-       }
-       if (typeof settings.renderNode === 'undefined') {
-           throw new Error('/dev.js not find renderNode')
-       }
+       let App = typeof Component.default !== 'undefined'? Component.default :Component
+       if (typeof settings.renderNode === 'undefined') { throw new Error('/dev.js not find renderNode') }
        render(<App {...settings.props} />, settings.renderNode)
     }
 }
